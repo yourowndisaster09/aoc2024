295c0fe26cd76631e7f11606a18cab862fd669d7
rbustos <roberta.bustos@rockettravel.com>
2024-12-05 10:16:22 -0600

    test

--- 05/submit.py
+++ 05/submit.py
@@ -46,6 +46,32 @@ def part1(rules, updates):
     return answer
 
 
+def part2(rules, updates):
+    answer = 0
+    for update in updates:
+        n = len(update)
+        adj = {}
+        for x, y in rules:
+            if x not in update or y not in update:
+                continue
+            if x not in adj:
+                adj[x] = []
+            if y not in adj:
+                adj[y] = []
+            adj[x].append(y)
+        TS = topologicalSort(adj)
+
+        # Just my assumption for this problem - all pages have rules; otherwise we have multiple possible TS
+        if len(TS) != len(update):
+            assert False
+
+        if TS != update:
+            # print(update, TS)
+            answer += int(TS[n // 2])
+        
+    return answer
+
+
 if __name__ == "__main__":
     rules = []
     updates = []
@@ -60,3 +86,5 @@ if __name__ == "__main__":
         # print(all(([len(x) % 2 == 1 for x in updates])))
         if sys.argv[2] == "part1":
             print(f'Part 1 Answer = {part1(rules, updates)}')
+        if sys.argv[2] == "part2":
+            print(f'Part 2 Answer = {part2(rules, updates)}')
