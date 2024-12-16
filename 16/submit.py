d1965ed9b6c2edcfdf92ad31430e1e2f7a81b76c
Roberta Bustos <rbbustos3@gmail.com>
2024-12-16 01:46:52 -0600

    day 16

--- 16/submit.py
+++ 16/submit.py
@@ -1,9 +1,10 @@
 import sys
 from math import inf
 from heapq import *
+from collections import deque
 
 def part1(map, start, end):
-    print(start, end)
+    # print(start, end)
     m = len(map)
     n = len(map[0])
 
@@ -35,7 +36,7 @@ def part1(map, start, end):
         ]
 
         for nR, nC, nO, addedScore in nextSteps:
-            print(nR, nC, nO)
+            # print(nR, nC, nO)
             if nR > -1 and nR < m and nC > -1 and nC < n and not V[nR][nC][nO] and map[nR][nC] != '#':
                 D[nR][nC][nO] = min(
                     D[nR][nC][nO],
@@ -46,7 +47,6 @@ def part1(map, start, end):
 
 
 def part2(map, start, end):
-    print(start, end)
     m = len(map)
     n = len(map[0])
 
@@ -57,36 +57,70 @@ def part2(map, start, end):
         3: (0, -1)  # LEFT
     }
 
-    V = [[[False for _ in range(4)] for _ in range(n)] for _ in range(m)]
-    D = [[[inf for _ in range(4)] for _ in range(n)] for _ in range(m)]
-    C = [[[0 for _ in range(4)] for _ in range(n)] for _ in range(m)]
-    D[start[0]][start[1]][1] = 0
+    V = [[False for _ in range(4) for _ in range(n)] for _ in range(m)]
+    D = [[inf for _ in range(4) for _ in range(n)] for _ in range(m)]
+    D[start[0]][start[1]] = 0
     pq = [(0, start, 1)]
 
     while pq:
         score, pos, o = heappop(pq)
         r, c = pos
 
-        if V[r][c][o]:
+        if V[r][c]:
             continue
-        V[r][c][o] = True
+        V[r][c] = True
 
-        nextSteps = [
-            (r, c, (o + 1) % 4, 1000),
-            (r, c, (o + 2) % 4, 1000),
-            (r, c, (o + 3) % 4, 1000),
-            (r + VECTOR[o][0], c + VECTOR[o][1], o, 1),
-        ]
-
-        for nR, nC, nO, addedScore in nextSteps:
-            print(nR, nC, nO)
-            if nR > -1 and nR < m and nC > -1 and nC < n and not V[nR][nC][nO] and map[nR][nC] != '#':
-                D[nR][nC][nO] = min(
-                    D[nR][nC][nO],
+        for nO, val in VECTOR.items():
+            dR, dC = val
+            nR = r + dR
+            nC = c + dC
+            if nR > -1 and nR < m and nC > -1 and nC < n and not V[nR][nC] and map[nR][nC] != '#':
+                addedScore = 1
+                if o != nO:
+                    addedScore += 1000
+                D[nR][nC] = min(
+                    D[nR][nC],
                     score + addedScore
                 )
-                heappush(pq, (D[nR][nC][nO], (nR, nC), nO))
-    return min(D[end[0]][end[1]])
+                heappush(pq, (D[nR][nC], (nR, nC), nO))
+    # print(D[end[0]][end[1]])
+
+    # BFS back to S
+    V = [[False for _ in range(n)] for _ in range(m)]
+    q = deque([(end, None)])
+    while q:
+        pos, prev = q.popleft()
+        r, c = pos
+        if V[r][c]:
+            continue
+        V[r][c] = True
+        if pos == start:
+            continue
+
+        realScore = {}
+        for o in range(4):
+            reverseO = (o + 2) % 4
+            nR = r + VECTOR[reverseO][0]
+            nC = c + VECTOR[reverseO][1]
+            if nR > -1 and nR < m and nC > -1 and nC < n and not V[nR][nC]:
+                realScore[o] = D[nR][nC]
+                if prev is not None and prev != o:
+                    realScore[o] += 1000
+        realMin = min(realScore.values())
+        for o, score in realScore.items():
+            if score == realMin:
+                reverseO = (o + 2) % 4
+                nR = r + VECTOR[reverseO][0]
+                nC = c + VECTOR[reverseO][1]
+                q.append(((nR, nC), o))
+
+    count = 0
+    for row in V:
+        for val in row:
+            if val:
+                count += 1
+    #[print([1 if x else 0 for x in v]) for v in V]
+    return count
 
 
 if __name__ == "__main__":
