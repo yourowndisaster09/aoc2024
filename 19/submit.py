da3a6b157fb26d35a764df216e7aa193f0f28961
rbustos <roberta.bustos@rockettravel.com>
2024-12-18 16:16:54 -0600

    day 19

---
+++ 19/submit.py
@@ -0,0 +1,28 @@
+import sys
+from math import *
+from time import *
+from collections import *
+from heapq import *
+
+
+def part1(a):
+    pass
+
+
+def part2(a):
+    pass
+
+
+if __name__ == "__main__":
+    data = []
+    with open(sys.argv[1], 'r') as f:
+        for raw_line in f:
+            line = raw_line.strip()
+            if not line:
+                break
+            data.append(line)
+
+    if sys.argv[2] == "part1":
+        print(f'Part 1 Answer = {part1(data)}')
+    if sys.argv[2] == "part2":
+        print(f'Part 2 Answer = {part2(data)}')
