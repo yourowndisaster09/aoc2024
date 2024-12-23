ab17f701d28f0bf77befa7bd2bcd082de89c6b5d
Roberta Bustos <rbbustos3@gmail.com>
2024-12-22 23:31:03 -0600

    answers

--- 23/submit.py
+++ 23/submit.py
@@ -6,12 +6,59 @@ from heapq import *
 
 
 def part1(data):
-    print(data)
-    pass
+    adj = defaultdict(set)
+    for line in data:
+        a, b = line.split('-')
+        adj[a].add(b)
+        adj[b].add(a)
+
+    comps = set()
+    for k, connections in adj.items():
+        n = len(connections)
+        listCon = list(connections)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if listCon[j] in adj[listCon[i]]:
+                    comps.add(tuple(sorted([k, listCon[i], listCon[j]])))
+    # print(len(comps))
+    count = 0
+    for c in comps:
+        hasT = False
+        for t in c:
+            if t[0] == 't':
+                hasT = True
+                break
+        if hasT:
+            count += 1
+    return count
 
 
 def part2(data):
-    pass
+    adj = defaultdict(set)
+    for line in data:
+        a, b = line.split('-')
+        adj[a].add(b)
+        adj[b].add(a)
+    maxLen = 0
+    validList = None
+    for k, connections in adj.items():
+        n = len(connections)
+        listCon = list(connections)
+        for i in range(n):
+            valid = set([k])
+            valid.add(listCon[i])
+            for j in range(i + 1, n):
+                add = True
+                for v in valid:
+                    if listCon[j] not in adj[v]:
+                        add = False
+                        break
+                if add:
+                    valid.add(listCon[j])
+            if len(valid) > maxLen:
+                validList = valid
+                maxLen = len(valid)
+    return ','.join(sorted(validList))
 
 
 if __name__ == "__main__":
